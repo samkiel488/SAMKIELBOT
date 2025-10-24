3a6fa505051a0eb37d7443cd80b0ee2b4217dcda
SAMKIEL <samuelezekiel488@gmail.com>
2025-10-24 19:51:23 +0100

    delete 1 file and update 22 files

--- frontend/.next/dev/server/middleware-build-manifest.js
+++ frontend/.next/dev/server/middleware-build-manifest.js
@@ -30,21 +30,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/[root-of-the-server]__d709e37a._.js",
       "static/chunks/pages_dashboard_2da965e7._.js",
       "static/chunks/turbopack-pages_dashboard_97053aaf._.js"
-    ],
-    "/login": [
-      "static/chunks/node_modules_next_dist_compiled_c629999b._.js",
-      "static/chunks/node_modules_next_dist_shared_lib_2202bc1a._.js",
-      "static/chunks/node_modules_next_dist_client_3ede7da4._.js",
-      "static/chunks/node_modules_next_dist_6024eba3._.js",
-      "static/chunks/node_modules_next_e5450561._.js",
-      "static/chunks/node_modules_react_e3593a73._.js",
-      "static/chunks/node_modules_react-dom_cjs_react-dom_development_2b5e0eb3.js",
-      "static/chunks/node_modules_react-dom_8a8085df._.js",
-      "static/chunks/node_modules_react-icons_27a67710._.js",
-      "static/chunks/node_modules_8c14007f._.js",
-      "static/chunks/[root-of-the-server]__c9558bbc._.js",
-      "static/chunks/pages_login_2da965e7._.js",
-      "static/chunks/turbopack-pages_login_959e4707._.js"
     ]
   },
   "devFiles": [],
