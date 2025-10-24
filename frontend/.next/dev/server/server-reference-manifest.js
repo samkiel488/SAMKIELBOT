f8241f2713aee915f01ed86deb6f748b2d482dbd
SAMKIEL <samuelezekiel488@gmail.com>
2025-10-24 19:25:46 +0100

    create 21 files

---
+++ frontend/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Dus1G0PPsgODyTxGGvSUfgtzjZMJ8l3Jod+9gJ1BMyQ=\"\n}"
---
+++ frontend/.next/dev/static/development/_buildManifest.js
@@ -0,0 +1,15 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/dashboard",
+    "/login",
+    "/register"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
